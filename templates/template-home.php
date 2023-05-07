0c0c7b994ca4024637e2f67b9c7dbe19b39d4600
ateslya <ateslya@crispwp.com>
2023-05-07 15:23:26 +0300

    add get_template_part into home

--- templates/template-home.php
+++ templates/template-home.php
@@ -7,6 +7,24 @@ Template name: Home
 */ ?>
 <?php get_header(); ?>
 <main>
-    <?php //get_template_part( 'acf-blocks/hero/index' ); ?>
+    <?php
+    get_template_part( 'acf-blocks/blog-post/index' );
+    get_template_part( 'acf-blocks/columns-cards/index' );
+    get_template_part( 'acf-blocks/counter/index' );
+    get_template_part( 'acf-blocks/cta/index' );
+    get_template_part( 'acf-blocks/faqs/index' );
+    get_template_part( 'acf-blocks/gallery/index' );
+    get_template_part( 'acf-blocks/hero/index' );
+    get_template_part( 'acf-blocks/masonry/index' );
+    get_template_part( 'acf-blocks/news/index' );
+    get_template_part( 'acf-blocks/parallax/index' );
+    get_template_part( 'acf-blocks/partners/index' );
+    get_template_part( 'acf-blocks/quote/index' );
+    get_template_part( 'acf-blocks/scrollbar/index' );
+    get_template_part( 'acf-blocks/tabs/index' );
+    get_template_part( 'acf-blocks/team/index' );
+    get_template_part( 'acf-blocks/two-columns/index' );
+    get_template_part( 'acf-blocks/video/index' );
+    ?>
 </main>
 <?php get_footer(); ?>
